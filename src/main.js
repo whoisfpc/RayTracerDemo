fa5a5f8a898c6c88f709f191831bef9b9d163088
wo_fpc <wo_fpc@outlook.com>
2017-12-18 11:11:03 +0800

    light

--- src/main.js
+++ src/main.js
@@ -5,10 +5,40 @@ class Vector3 {
      * @param {number} z 
      */
     constructor(x, y, z) {
-        this.value = [x, y, z]
         this.x = x
         this.y = y
         this.z = z
+    }
+
+    /**
+     * return a zero vector
+     * @returns {Vector3}
+     */
+    static zero() {
+        return new Vector3(0, 0, 0)
+    }
+
+    /**
+     * return a one vector
+     * @returns {Vector3}
+     */
+    static one() {
+        return new Vector3(1, 1, 1)
+    }
+
+    /**
+     * @returns {number}
+     */
+    lenght() {
+        return Math.sqrt(this.dot(this))
+    }
+
+    /**
+     * return a normalized vector of this
+     * @returns {Vector3}
+     */
+    normalized() {
+        return this.scale(1/this.lenght())
     }
 
     /**
@@ -30,6 +60,23 @@ class Vector3 {
     }
 
     /**
+     * mul two vector3 return a new vector3
+     * @param {Vector3} v 
+     */
+    mul(v) {
+        return new Vector3(this.x * v.x, this.y * v.y, this.z * v.z)
+    }
+
+    /**
+     * scale vector with t and return a new vector
+     * @param {number} t 
+     * @returns {Vector3}
+     */
+    scale(t) {
+        return new Vector3(this.x * t, this.y * t, this.z * t)
+    }
+
+    /**
      * dot two vector3 return a number
      * @param {Vector3} v 
      * @returns {number}
@@ -43,7 +90,7 @@ class Sphere {
     /**
      * @param {Vector3} center 
      * @param {number} radius 
-     * @param {?} color 
+     * @param {Vector3} color 
      */
     constructor(center, radius, color) {
         this.center = center
@@ -52,9 +99,34 @@ class Sphere {
     }
 }
 
+const LightType = {
+    ambient: 0,
+    point: 1,
+    directional: 2
+}
+
+class Light {
+    /**
+     * 
+     * @param {number} type 
+     * @param {number} intensity 
+     * @param {Vector3} position 
+     * @param {Vector3} direction 
+     */
+    constructor(type, intensity, position, direction) {
+        this.type = type
+        this.intensity = intensity
+        this.position = position || Vector3.zero()
+        this.direction = direction || Vector3.one()
+    }
+}
+
 class Scene {
     constructor() {
+        /** @type {Sphere[]} */
         this.spheres = []
+        /** @type {Light[]} */
+        this.lights = []
     }
 
     /**
@@ -63,6 +135,15 @@ class Scene {
      */
     addSphere(sphere) {
         this.spheres.push(sphere)
+        return this
+    }
+
+    /**
+     * @param {Light} light 
+     * @returns {Scene}
+     */
+    addLight(light) {
+        this.lights.push(light)
         return this
     }
 }
@@ -85,6 +166,12 @@ function v3(x, y, z) {
     const buffer = ctx.getImageData(0, 0, canvas.width, canvas.height)
     const pitch = buffer.width * 4;
 
+    /**
+     * put a pixel on (x, y) with color
+     * @param {number} x 
+     * @param {number} y 
+     * @param {Vector3} color 
+     */
     function putPixel(x, y, color) {
         x = canvas.width * 0.5 + x
         y = canvas.height * 0.5 - y
@@ -94,20 +181,24 @@ function v3(x, y, z) {
         }
 
         const offset = x * 4 + y * pitch
-        buffer.data[offset] = color[0]
-        buffer.data[offset+1] = color[1]
-        buffer.data[offset+2] = color[2]
+        buffer.data[offset] = color.x
+        buffer.data[offset+1] = color.y
+        buffer.data[offset+2] = color.z
         buffer.data[offset+3] = 255
     }
 
     const scene = new Scene()
-    scene.addSphere(new Sphere(v3(0, -1, 3), 1, [255, 0, 0]))
-        .addSphere(new Sphere(v3(2, 0, 4), 1, [0, 0, 255]))
-        .addSphere(new Sphere(v3(-2, 0, 4), 1, [0, 255, 0]))
+    scene.addSphere(new Sphere(v3(0, -1, 3), 1, v3(255, 0, 0)))
+        .addSphere(new Sphere(v3(2, 0, 4), 1, v3(0, 0, 255)))
+        .addSphere(new Sphere(v3(-2, 0, 4), 1, v3(0, 255, 0)))
+        .addSphere(new Sphere(v3(0, -5001, 0), 5000, v3(255, 255, 0)))
+        .addLight(new Light(LightType.ambient, 0.2))
+        .addLight(new Light(LightType.point, 0.6, v3(2, 1, 0)))
+        .addLight(new Light(LightType.directional, 0.2, null, v3(1, 4, 4)))
     const cameraPosition = v3(0, 0, 0)
     const viewportSize = 1
     const projectionDepth = 1
-    const backgroundColor = [255, 255, 255]
+    const backgroundColor = v3(255, 255, 255)
 
     /**
      * convert canvas coord to viewport coord
@@ -124,6 +215,7 @@ function v3(x, y, z) {
      * @param {Vector3} o 
      * @param {Vector3} dir 
      * @param {Sphere} sphere 
+     * @returns {Array}
      */
     function intersetRaySphere(o, dir, sphere) {
         const oc = o.sub(sphere.center)
@@ -141,8 +233,44 @@ function v3(x, y, z) {
         return [t1, t2]
     }
 
+    /**
+     * compute light intensity at point `p`
+     * @param {Vector3} p 
+     * @param {Vector3} n 
+     * @returns {number}
+     */
+    function computeLighting(p, n) {
+        let intensity = 0
+        scene.lights.forEach(light => {
+            if (light.type === LightType.ambient) {
+                intensity += light.intensity
+            } else {
+                let l
+                if (light.type === LightType.point) {
+                    l = light.position.sub(p)
+                } else {
+                    l = light.direction
+                }
+                const ndotl = n.dot(l)
+                if (ndotl > 0) {
+                    intensity += light.intensity * ndotl / (n.lenght() * l.lenght())
+                }
+            }
+        })
+        return intensity
+    }
+
+    /**
+     * 
+     * @param {Vector3} o 
+     * @param {Vector3} dir 
+     * @param {number} tMin 
+     * @param {number} tMax 
+     * @returns {Vector3}
+     */
     function traceRay(o, dir, tMin, tMax) {
         let closestT = Infinity
+        /** @type {Sphere} */
         let closestSphere = null
         scene.spheres.forEach(sphere => {
             const t = intersetRaySphere(o, dir, sphere)
@@ -158,15 +286,22 @@ function v3(x, y, z) {
         if (closestSphere == null) {
             return backgroundColor
         }
-        return closestSphere.color
+        const p = o.add(dir.scale(closestT))
+        const n = p.sub(closestSphere.center).normalized()
+        return closestSphere.color.scale(computeLighting(p, n))
     }
 
+    function clamp(color) {
+        return v3(Math.min(255, Math.max(0, color.x)),
+            Math.min(255, Math.max(0, color.y)),
+            Math.min(255, Math.max(0, color.z)),)
+    }
 
     for (let x = -canvas.width / 2; x < canvas.width / 2; x++) {
         for (let y = -canvas.height / 2 + 1; y <= canvas.height / 2; y++) {
             let viewportCoord = canvasToViewport(x, y)
             let color = traceRay(cameraPosition, viewportCoord.sub(cameraPosition), 1, Infinity)
-            putPixel(x, y, color)
+            putPixel(x, y, clamp(color))
         }
     }
 
